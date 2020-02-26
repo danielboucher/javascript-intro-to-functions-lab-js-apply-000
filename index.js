8904976bc54612c6c068ff29440f6da29713f7c9
Daniel Boucher <dtboucher@gmail.com>
2020-02-26 04:41:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-function shout(string) {
-  return string.toUpperCase();
-}
-function whisper(string) {
-  return string.toLowerCase();
-}
-function logShout(string) {
-  console.log(string.toUpperCase());
-}
-function logWhisper(string) {
-  console.log(string.toLowerCase());
-}
+// function shout(string) {
+//   return string.toUpperCase();
+// }
+// function whisper(string) {
+//   return string.toLowerCase();
+// }
+// function logShout(string) {
+//   console.log(string.toUpperCase());
+// }
+// function logWhisper(string) {
+//   console.log(string.toLowerCase());
+// }
 
 var lowercase = 'hello!';
 var mixedcase = "'I love you, Grandma. ";
 var uppercase = "HELLO!";
 
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma() {
   // for input string to function sayHiToGrandma,
   // if string is equal to string in all lowercase, return string "I can't hear you!"
 
-    if (lowercase.toLowerCase() === lowercase) {
-        console.log(`I can't hear you!`);
-    } else if (uppercase.toUpperCase(s) === uppercase)
-        console.log("YES INDEED!");
-    // } else (mixedcase.toLowerCase() !== mixedcase)
-        // console.log("I love you, too.");
+  lowercase.toLowerCase() === lowercase);
+        return (`I can't hear you!`);
+  uppercase.toUpperCase() === uppercase;
+        return ("YES INDEED!");
+  mixedcase.toLowerCase() !== mixedcase;
+        return ("I love you, too.");
   }
 
 //   if (lowercase.toLowerCase() === lowercase) {
