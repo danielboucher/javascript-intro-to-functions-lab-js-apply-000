88738aa726c010b027050da6ae1c9ad9008ca903
Daniel Boucher <dtboucher@gmail.com>
2020-02-26 05:29:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-// var lowercase = 'hello!';
-// var mixedcase = "I love you, Grandma.";
-// var uppercase = "HELLO!";
-
 function sayHiToGrandma(greeting) {
   // compare greeting with greeting in all uppercase
   if (greeting === "I love you, Grandma.") {
       return "I love you, too.";
   // otherwise if greeting is all uppercase compare to greeting if true return
-  } else if (greeting.toUpperCase() === greeting) {
+} else if (shout(greeting) === greeting) {
       return "YES INDEED!";
   // otherwise if greeting is all lowercase return
-  } else if (greeting.toLowerCase() === greeting) {
+} else if (whisper(greeting) === greeting) {
       return "I can't hear you!";
   }
 }
